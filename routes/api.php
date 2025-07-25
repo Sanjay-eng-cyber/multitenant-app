4451ac4cab01f3bf7f641468e4d1fa1a37e2ca2c
sanjay-52-tv <sanjayc.studio52@gmail.com>
2025-07-25 17:51:45 +0530

    env addes

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,11 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\InvoiceController;
 use App\Http\Controllers\ProjectController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 
